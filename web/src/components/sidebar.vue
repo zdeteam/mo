9e134d4cceb7f1aa3dfb7d9043502f73bda9dbc4
Superme <102033643+supermez@users.noreply.github.com>
2022-06-09 13:31:28 +0800

    Update sidebar.vue

--- web/src/components/sidebar.vue
+++ web/src/components/sidebar.vue
@@ -105,7 +105,7 @@ import {
 } from '@vicons/ionicons5';
 import { Hash } from '@vicons/tabler';
 import { getUnreadMsgCount } from '@/api/user';
-import LOGO from '@/assets/img/logo.png';
+import LOGO from '@/assets/img/logo.svg';
 
 const store = useStore();
 const route = useRoute();
